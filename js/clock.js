c4878c1e8a2a58c75d23e76e120e758500fe5d9e
juanjo-laptop <juanjo.l.seoane@gmail.com>
2022-10-21 16:24:05 +0200

    to Javascript ES6

--- js/clock.js
+++ js/clock.js
@@ -1,21 +1,20 @@
 let now = new Date()
-document.getElementById('mapupdate').innerHTML = `${now.toLocaleString()} <p id="clock"></p>`;
+document.getElementById('mapupdate').innerHTML = `${now.toLocaleString()} <p id="clock"></p>`
 
-var inicialTime = 300; // actualiza cada 5 minutos
-var actualTime = inicialTime;
+var inicialTime = 300 // actualiza cada 5 minutos
+var actualTime = inicialTime
 
 function Clock() {
 
     if (actualTime == 0) {
-        document.location.reload();
-        actualTime = inicialTime;
+        document.location.reload()
+        actualTime = inicialTime
     } else {
-        actualTime -= 1;
-        setTimeout("Clock()", 1000); // 1 seg
+        actualTime -= 1
+        setTimeout("Clock()", 1000) // 1 seg
     }
-    var minutos = Math.floor(actualTime/60) + 1;
-    document.getElementById('clock').innerHTML = minutos
-        + " min. para actualización";
+    var minutos = Math.floor(actualTime/60) + 1
+    document.getElementById('clock').innerHTML = `${minutos} min. para actualización`
 }
 
-Clock();
+Clock()
--- js/map.js
+++ js/map.js
@@ -1,14 +1,14 @@
 
-let map = null;
+let map = null
 
-loadAemet();
+loadAemet()
 
 function loadMap(aemet) {
 
 	var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	});
+		attribution: '&copy <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	})
 
 
 	// Centro en Madrid
@@ -16,47 +16,46 @@ function loadMap(aemet) {
 		center: [40.4381311, -3.8196201],
 		zoom: 6,
 		layers: [tiles, aemet]
-	});
+	})
 
 	// Mapas base, se pueden ampliar con otras fuentes
 	var baseMaps = {
 		"OpenStreetMap": tiles,
-	};
+	}
 
 	// Capa con la imagen de la Aemet
 	var overlayMaps = {
 		"Aemet": aemet,
-	};
+	}
 
-	L.control.layers(baseMaps, overlayMaps).addTo(map);
+	L.control.layers(baseMaps, overlayMaps).addTo(map)
 }
 
 function loadAemet() {
-	// Define API Key
-	var AK = "";
-	// Define request URLs
-	var URL1 = "https://opendata.aemet.es/opendata/api/red/radar/nacional";
+	// API Key obtenida en Opendata Aemet
+	var AK = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWFuam8ubC5zZW9hbmVAZ21haWwuY29tIiwianRpIjoiZDE3M2QzZGYtNGUxMC00NDdkLThmNTYtMTBiMTc5YjNlY2U5IiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2NDk0MjI5NTYsInVzZXJJZCI6ImQxNzNkM2RmLTRlMTAtNDQ3ZC04ZjU2LTEwYjE3OWIzZWNlOSIsInJvbGUiOiIifQ.vf8BhVgGFF82flCWcoZ0tyP8yRQ7uXnGNC9k1Yc6ng0" 
+	// Define URLs de OpenData Aemet
+	var URL1 = "https://opendata.aemet.es/opendata/api/red/radar/nacional"
 
-	var URL2 = "";
+	var URL2 = ""
 
-	// Create XMLHttpRequest objects
-	const xhttp1 = new XMLHttpRequest();
-	const xhttp2 = new XMLHttpRequest();
+	// XMLHttpRequest
+	const xhttp1 = new XMLHttpRequest()
 
-	// Define response function for the first request:
-	// We want to access te "datos" URL
+	// Primer Request
+	// Devuelve un objeto con formato JSON. El parámetro "datos" contiene la URL de la imagen del radar
 	xhttp1.onload = function () {
-		// 1º Parse the response to JSON:
-		URL2 = JSON.parse(this.responseText);
-		// 2º Get the "datos" field:
-		var imageUrl = URL2.datos;
-		app.loadPicture(imageUrl);
+		// Parse a JSON:
+		URL2 = JSON.parse(this.responseText)
+		// Obtiene el campo "datos"
+		var imageUrl = URL2.datos
+		app.loadPicture(imageUrl)
 
 	}
 
-	// Send the first request
-	xhttp1.open("GET", URL1 + "/?api_key=" + AK);
-	xhttp1.send();
+	// Send Request
+	xhttp1.open("GET", URL1 + "/?api_key=" + AK)
+	xhttp1.send()
 }
 
 // Aemet app.loadPicture
@@ -65,60 +64,59 @@ var app = (function () {
 		context = canvas.getContext('2d'),
 
 		// API
-		public = {};
+		public = {}
 
-	// Public methods goes here...
 	public.loadPicture = function (imageUrl) {
-		var imageObj = new Image();
+		var imageObj = new Image()
 		imageObj.src = imageUrl
-		imageObj.crossOrigin = ""
+		// URL que va a originar la petición de datos
+		imageObj.crossOrigin = "https://aol.orbisnonsufficit.es/"
 
 		imageObj.onload = function () {
-			context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
-			app.filters.opacity();
+			context.drawImage(imageObj, 0, 0, canvas.width, canvas.height)
+			app.filters.opacity()
 		}
-	};
+	}
 
 	public.getImgData = function () {
-		return context.getImageData(0, 0, canvas.width, canvas.height);
-	};
+		return context.getImageData(0, 0, canvas.width, canvas.height)
+	}
 
 	// Filters
-	public.filters = {};
+	public.filters = {}
 
-	// Filtro para aplicar opacidad 0 donde interesa
+	// Filtro para aplicar transparencia
 	public.filters.opacity = function () {
 		var imageData = app.getImgData(),
 			pixels = imageData.data,
-			numPixels = imageData.width * imageData.height;
+			numPixels = imageData.width * imageData.height
 
 		for (var i = 0; i < numPixels; i++) {
-			var r = pixels[i * 4];
-			var g = pixels[i * 4 + 1];
-			var b = pixels[i * 4 + 2];
-			// var a = pixels[i * 4 + 3];
+			var r = pixels[i * 4]
+			var g = pixels[i * 4 + 1]
+			var b = pixels[i * 4 + 2]
 
 			if (r == 127 // gris
 				|| (r == 0 && g == 0 && b == 0) // negro
 				|| (i <= 34200) // primera filas con logo de Aemet
 				|| (i >= 543400) // últimas filas con leyenda
 				|| (r == 255 && g == 0 && b == 255)) { // contorno rosado del mapa
-				pixels[i * 4] = 255; // rgb a blanco
-				pixels[i * 4 + 1] = 255; // rgb a blanco
-				pixels[i * 4 + 2] = 255; // rgb a blanco
-				pixels[i * 4 + 3] = 0; // opacidad 0 para convertirlo en transparente
+				pixels[i * 4] = 255 // rgb a blanco
+				pixels[i * 4 + 1] = 255 // rgb a blanco
+				pixels[i * 4 + 2] = 255 // rgb a blanco
+				pixels[i * 4 + 3] = 0 // opacidad 0 para convertirlo en transparente
 			}
 		}
 
-		context.putImageData(imageData, 0, 0);
-		var aemetPng = canvas.toDataURL('image/png');
+		context.putImageData(imageData, 0, 0)
+		var aemetPng = canvas.toDataURL('image/png')
 
 		// Marco donde se ubicará la imagen
 		latLngBounds = [
 			[46.4, -2.0], // límite NORTE
 			[41.0, -12.0], // límite OESTE
 			[32.9, 0.0], // límite SUR
-			[40.419623569820125, 5.859104861536971]]; // límite ESTE
+			[40.419623569820125, 5.859104861536971]] // límite ESTE
 
 		var imageOverlay = L.imageOverlay(aemetPng, latLngBounds, {
 			opacity: 1,
@@ -126,9 +124,9 @@ var app = (function () {
 			interactive: true,
 		})
 
-		aemet = L.layerGroup([imageOverlay]);
-		loadMap(aemet);
-	};
+		aemet = L.layerGroup([imageOverlay])
+		loadMap(aemet)
+	}
 
-	return public;
-}());
+	return public
+}())
